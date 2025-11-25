b4ca2888c4c930734fe4cacba8214a5c5893b161
Shiyam-p <shiyam9904@gmail.com>
2025-11-25 23:01:25 +0530

    modify the forEach html & js file

---
+++ forEach.js
@@ -0,0 +1,7 @@
+// forEach() is an array method that runs a function once for every element in the array.
+
+let ipl = ["rcb","csk","mi","srh","kkr","gt"]
+
+ipl.forEach((iplTeam) => {
+    console.log(iplTeam.toUpperCase());
+});
