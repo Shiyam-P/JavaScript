84565f53d408ffd034dbcae8b07728d891836abe
Shiyam-p <shiyam9904@gmail.com>
2025-11-26 21:35:17 +0530

    modify the map.html and map.css file

---
+++ map.js
@@ -0,0 +1,21 @@
+// The map() method creates a new array by applying a callback function to each element of the original array.
+// It does not modify the original array
+
+//Example: 1
+let arr = ["apple","banana","mango"];
+let res = arr.map((num) => {
+    // console.log(num.toUpperCase());
+    return num.toUpperCase();
+});
+console.log(res);
+
+//Example: 2
+let number1 = [1,2,3,4]
+number1.map((num2) => {
+    console.log(num2);   
+})
+
+//Example: 3
+let number = [1,2,3,4]
+let double = number.map((num1) => num1 * 2)
+console.log(double);
